7c318cd9517042aed9bf1cd660f45c69403a0424
César Villar <cesar.villar@kairosds.com>
2019-03-01 17:25:40 +0100

    bypassed other props to mediaObject, added proptypes

--- src/components/MediaObject/MediaObject.js
+++ src/components/MediaObject/MediaObject.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './MediaObject.css';
 import Text from '../Text';
+import propTypes from 'prop-types';
 
-const MediaObject = ({imgSrc, text, vertical = false}) => (
-  <div className={`mediaObject${vertical ? ' vertical' : ''}`}>
+const MediaObject = ({imgSrc, text, vertical = false, ...props}) => (
+  <div className={`mediaObject${vertical ? ' vertical' : ''}`} {...props}>
     <img src={imgSrc} />
     <Text.Regular className="text" children={text} />
   </div>
 );
 
+MediaObject.propTypes = {
+  imgSrc: propTypes.string.isRequired,
+  text: propTypes.string.isRequired,
+  vertical: propTypes.bool,
+};
 export default MediaObject;
